5d5dc3ef83f48fbbf12b164bec093a01a5278269
johnston9234 <rjohn206@uncc.edu>
2015-10-15 18:38:26 +0000

    completed section 4.4

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,11 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+ resources :movies
+
+
+  root :to => redirect( '/movies')
+  
   # Example resource route with options:
   #   resources :products do
   #     member do
